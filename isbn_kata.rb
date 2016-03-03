7f10b271fab6243f183c99302161dd897acf6e84
jennifer Grudi <jen.grudi@gmail.com>
2016-03-02 21:47:37 -0500

    refactor

--- isbn_kata.rb
+++ isbn_kata.rb
@@ -1,5 +1,10 @@
-def valid_isbn?(isbn_input)
+def delete_spaces_and_hyphens(isbn_input)	
 	string_without_spaces = isbn_input.delete(" ")
 	string_without_spaces_or_hyphens = string_without_spaces.delete("-")
+end	
+
+def valid_isbn?(isbn_input)
+	string_without_spaces_or_hyphens = delete_spaces_and_hyphens(isbn_input)
 	string_without_spaces_or_hyphens.length == 10
-end		
+end	
+
